0a1fedcbb1bf4af37a16a1d782b5cd62c193d4dd
Matthew Moran <mmoran11@friars.providence.edu>
2023-10-17 00:51:33 -0400

    new functions added, functions filled

--- MinesweeperGame/Source.cpp
+++ MinesweeperGame/Source.cpp
@@ -6,7 +6,6 @@
 #include <string>
 using namespace std;
 
-int row, col;
 int const diff = GetDifficulty();
 string hiddenBoard[diff][diff];
 string board[diff][diff];
@@ -14,6 +13,7 @@ string board[diff][diff];
 
 void CreateTable() {
 	int startRow, startCol;
+	cout << "MINESWEEPER" << endl;
 	cout << "Select a starting position: " << endl;
 	cout << "Row: " << endl;
 	cin >> startRow;
@@ -21,7 +21,19 @@ void CreateTable() {
 	cin >> startCol;
 	for (int i = 0; i < diff; i++) {
 		for (int j = 0; j < diff; j++) {
-			if (rand() % 2 == 0 && i != startRow and j != startCol) {
+
+			// the starting position should have no adj mines
+
+			if (rand() % 3 == 0 && 
+				(i != startRow && j != startCol) &&
+				(i-1 != startRow && j-1 != startCol)&&
+				(i-1 != startRow && j != startCol)&&
+				(i-1 != startRow && j+1 != startCol)&&
+				(i != startRow && j+1 != startCol)&&
+				(i+1 != startRow && j+1 != startCol)&&
+				(i+1 != startRow && j != startCol)&&
+				(i+1 != startRow && j-1 != startCol)&&
+				(i != startRow && j-1 != startCol)) {
 				hiddenBoard[i][j] = '*';
 			}
 		}
@@ -29,31 +41,84 @@ void CreateTable() {
 	board = hiddenBoard;
 	for (int i = 0; i < diff; i++) {
 		for (int j = 0; j < diff; j++) {
-			hiddenBoard[i][j] = NumAdjMines(i, j);
+			hiddenBoard[i][j] = to_string(NumAdjMines(i, j));
 		}
 	}
+	cout << "Moves are made by entering a row and a column in the table." << endl;
 	DisplayBoard();
+	MovePrompt();
 }
 
-void MakeMove(int r, int c) {}
+void ShowZeros(int r, int c) {
+	//if you make a valid click, the adj boxes with 0 adj mines should also be shown
+	//need to check all 8 cases
+	if (hiddenBoard[r - 1][c - 1] == '0') {
+		board[r - 1][c - 1] = '0';
+	}
+	if (hiddenBoard[r-1][c] == '0') {
+		board[r-1][c] = '0';
+	}
+	if (hiddenBoard[r-1][c+1] == '0') {
+		board[r-1][c+1] = '0';
+	}
+	if (hiddenBoard[r][c+1] == '0') {
+		board[r][c+1] = '0';
+	}
+	if (hiddenBoard[r+1][c+1] == '0') {
+		board[r+1][c+1] = '0';
+	}
+	if (hiddenBoard[r+1][c] == '0') {
+		board[r+1][c] = '0';
+	}
+	if (hiddenBoard[r+1][c-1] == '0') {
+		board[r+1][c-1] = '0';
+	}
+	if (hiddenBoard[r][c-1] == '0') {
+		board[r][c-1] = '0';
+	}
+}
+
+void MovePrompt() {
+	int row, col;
+	cout << "Make a move:";
+	cout << "Row: " << endl;
+	cin >> row;
+	cout << "Column: " << endl;
+	cin >> col;
+	MakeMove(row, col);
+}
+
+void MakeMove(int r, int c) {
+	if (IsValid(r, c) == true) {
+		board[r][c] == hiddenBoard[r][c];
+		ShowZeros(r,c);
+		DisplayBoard();
+		MovePrompt();
+	}
+	else {
+		string response = "";
+		cout << "You lost!" << endl;
+		DisplayHiddenBoard();
+		cout << "Play again?" << endl;
+		cin >> response;
+		if (response == "Y" || response == "y" || response == "yes" || response == "Yes") {
+			CreateTable();
+		}
+	}
+
+}
 
 int GetDifficulty() {
 	string diff = "";
 	cout << "Select Difficulty: \n (1) Easy \n (2) Medium \n (3) Hard \n (4) Exit";
 	cin >> diff;
 	if (diff == "1" || diff == "Easy" || diff == "easy") {
-		row = 9;
-		col = 9;
 		return 9;
 	}
 	else if (diff == "2" || diff == "Medium" || diff == "medium") {
-		row = 20;
-		col = 20;
 		return 20;
 	}
 	else if (diff == "3" || diff == "Hard" || diff == "hard") {
-		row = 50;
-		col = 50;
 		return 50;
 	}
 	else if (diff == "4" || diff == "Exit" || diff == "exit") {
@@ -81,7 +146,16 @@ int DisplayHiddenBoard() {
 	}
 }
 
-bool IsValid(int r, int c) {}
+bool IsValid(int r, int c) {
+	if (NumAdjMines(r, c) == 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+
+
+}
 
 int NumAdjMines(int r, int c) {
 	int count = 0;
@@ -141,5 +215,5 @@ int NumAdjMines(int r, int c) {
 
 
 int main() {
-
+	CreateTable();
 }
