8c3446823ecafb527523ed9a62f7eec348c34f8f
Matthew Moran <mmoran11@friars.providence.edu>
2023-10-16 20:55:24 -0400

    Added functions

--- MinesweeperGame/Source.cpp
+++ MinesweeperGame/Source.cpp
@@ -1,2 +1,22 @@
 // This project creates a minesweeper game with multiple difficulites 
 // Created by Matthew Moran
+
+
+//Variables
+int row, col;
+int board;
+int hiddenBoard;
+
+void CreateTable() {}
+
+void MakeMove(int r, int c) {}
+
+void GetDifficulty() {}
+
+int DisplayBoard() {}
+
+int DisplayHiddenBoard() {}
+
+bool IsValid(int r, int c) {}
+
+int NumAdjMines(int r, int c) {}
